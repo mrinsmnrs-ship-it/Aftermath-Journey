ebdf5c7a14ab5c7025237ccc9fa8d9f77498acf0
mrinsmnrs-ship-it <mrinsmnrs@gmail.com>
2026-07-13 21:54:01 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AftermathDashboard from './components/Dashboard/AftermathDashboard.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AftermathDashboard />
+  </React.StrictMode>
+)
